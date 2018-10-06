3e2a9702e7b06a7b5bfedffd6867cf978f2efe3f
laughing.height <laughing.heights@gmail.com>
2018-10-06 17:02:24 +0100

    Testing CircleCi

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-    return <p>Welcome to Team Flairs!</p>;
+    return <p>Welcome to Team Flairs! Testing CircleCi</p>;
   }
 }
 
